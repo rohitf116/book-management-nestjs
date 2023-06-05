7ab766d9c40258c9076483263d37d9cc34e786bb
rohitf116 <rrsonawne090909@gmail.com>
2023-06-05 13:27:46 -0700

    books

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,37 @@
-export class CreateBookDto {}
+import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsArray,
+  IsISBN,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateBookDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(500)
+  excerpt: string;
+
+  userId: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsISBN()
+  ISBN: number;
+
+  @IsNotEmpty()
+  @IsString()
+  category: string;
+
+  @ArrayMinSize(1, { message: 'At least one string is required' })
+  @ArrayMaxSize(10, { message: 'Maximum of 10 strings allowed' })
+  @IsString({ each: true, message: 'Each item must be a string' })
+  subcategory: string[];
+}
